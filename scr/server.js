7226d5fab0b303bae75c5f36763966e7fe92f701
Tanish soni <tanishsoni0415@gmail.com>
2025-04-27 15:27:40 +0530

    last commit

--- scr/server.js
+++ scr/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import { router } from "./routes/ai.router.js";
 import cors from 'cors'
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use("/", router);
-server.use(express.static('build'));
+server.use(express.static(path.join(__dirname, "build")));
 server.get("/", (req, res) => {
   res.send("hello");
 });
